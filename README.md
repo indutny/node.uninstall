d63da46b63c48b8de95d5404d491512b0a365321
Fedor Indutny <fedor.indutny@gmail.com>
2010-11-18 11:08:24 +0600

    Initial release

---
+++ README.md
@@ -0,0 +1,26 @@
+# Node.Uninstall
+
+This is an npm package that will uninstall all other npm packages
+
+## Features
+
+  * Fast and reliable  
+
+## Browser Support
+
+Does it matter?
+
+## Usage
+
+Install npm: http://github.com/isaacs/npm
+Install some package (for example): npm install connect
+Install this one: npm install uninstall
+Check your packages list: npm list installed
+
+## Documentation
+
+Documentation is in the making. 
+
+## License
+
+Does it matter?
---
+++ uninstall.js
@@ -0,0 +1,55 @@
+var exec  = require('child_process').exec,
+    util = require('util'),
+    npm = require('npm'),
+    queue = [];
+    
+
+util.log('Installing module!');
+
+npm.commands.list(['installed'], function(err, modules) {
+ if (err) {
+    util.puts('Some error!');
+    util.puts(stderr);
+    return;
+  }
+  for (var name in modules) {
+    var module = modules[name];
+    
+    // Skip npm itself
+    if (name == 'npm' || name == 'uninstall') continue;
+    
+    for (var version in module.versions) {
+      if (module.versions[version].installed) {
+        queue.push(name);
+        break;
+      }
+    }
+  }
+  
+  processQueue();
+});
+
+function processQueue() {
+  var i = 0;
+  function next() {
+    if (i >= queue.length) {
+      util.puts('Work is done!');    
+      util.puts('So long, and thanks for all the fish!');
+      
+      return;
+    }
+    var module = queue[i++];
+    
+    npm.commands.uninstall(module, function(err) {
+      if (err) {
+        util.puts('Failed processing: ' + module);
+//      queue.push(module);
+        return next();
+      }
+      util.puts('Processed: ' + module);
+      next();
+    });
+  }
+  
+  next();
+}

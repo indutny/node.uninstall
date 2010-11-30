f28b44873d49b1e03d5fd2bd521044effda22d1c
isaacs <i@izs.me>
2010-11-29 21:45:13 -0800

    Fix the ^M chars, and avoid hitting the registry

--- uninstall.js
+++ uninstall.js
@@ -1,12 +1,11 @@
-var exec  = require('child_process').exec,
-    util = require('util'),
+var util = require('util'),
     npm = require('npm'),
     queue = [];
     
 
 util.log('Installing module!');
 
-npm.load({outfd: 0}, function(err) {
+npm.load({outfd: false, loglevel:"error", registry:null}, function(err) {
   if (err) {
     util.log('Failed to load npm');
     return;
@@ -34,29 +33,30 @@ npm.load({outfd: 0}, function(err) {
     
     processQueue();
   });
-
-  function processQueue() {
-    var i = 0;
-    function next() {
-      if (i >= queue.length) {
-        util.puts('Work is done!');    
-        util.puts('So long, and thanks for all the fish!');
-        
-        return;
-      }
-      var module = queue[i++];
-      
-      npm.commands.uninstall(module, function(err) {
-        if (err) {
-          util.puts('Failed processing: ' + module);
-  //      queue.push(module);
-          return next();
-        }
-        util.puts('Processed: ' + module);
-        next();
-      });
-    }
-    
-    next();
-  }
 });
+
+
+function processQueue() {
+  var i = 0;
+  function next() {
+    if (i >= queue.length) {
+      util.puts('Work is done!');    
+      util.puts('So long, and thanks for all the fish!');
+      
+      return;
+    }
+    var module = queue[i++];
+    
+    npm.commands.uninstall(module, function(err) {
+      if (err) {
+        util.puts('Failed processing: ' + module);
+//      queue.push(module);
+        return next();
+      }
+      util.puts('Processed: ' + module);
+      next();
+    });
+  }
+  
+  next();
+}

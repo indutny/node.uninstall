f208728216d4feeb44386588a585cdcf35188fb8
Fedor Indutny <fedor.indutny@gmail.com>
2010-11-30 11:25:41 +0600

    Show warning prompt, use npm.load

--- uninstall.js
+++ uninstall.js
@@ -6,50 +6,57 @@ var exec  = require('child_process').exec,
 
 util.log('Installing module!');
 
-npm.commands.list(['installed'], function(err, modules) {
- if (err) {
-    util.puts('Some error!');
-    util.puts(stderr);
+npm.load({}, function(err) {
+  if (err) {
+    util.log('Failed to load npm');
     return;
   }
-  for (var name in modules) {
-    var module = modules[name];
-    
-    // Skip npm itself
-    if (name == 'npm' || name == 'uninstall') continue;
-    
-    for (var version in module.versions) {
-      if (module.versions[version].installed) {
-        queue.push(name);
-        break;
-      }
-    }
-  }
   
-  processQueue();
-});
-
-function processQueue() {
-  var i = 0;
-  function next() {
-    if (i >= queue.length) {
-      util.puts('Work is done!');    
-      util.puts('So long, and thanks for all the fish!');
-      
+  npm.commands.list(['installed'], function(err, modules) {
+   if (err) {
+      util.puts('Some error!');
+      util.puts(stderr);
       return;
     }
-    var module = queue[i++];
-    
-    npm.commands.uninstall(module, function(err) {
-      if (err) {
-        util.puts('Failed processing: ' + module);
-//      queue.push(module);
-        return next();
+    for (var name in modules) {
+      var module = modules[name];
+      
+      // Skip npm itself
+      if (name == 'npm' || name == 'uninstall') continue;
+      
+      for (var version in module.versions) {
+        if (module.versions[version].installed) {
+          queue.push(name);
+          break;
+        }
       }
-      util.puts('Processed: ' + module);
-      next();
-    });
+    }
+    
+    processQueue();
+  });
+
+  function processQueue() {
+    var i = 0;
+    function next() {
+      if (i >= queue.length) {
+        util.puts('Work is done!');    
+        util.puts('So long, and thanks for all the fish!');
+        
+        return;
+      }
+      var module = queue[i++];
+      
+      npm.commands.uninstall(module, function(err) {
+        if (err) {
+          util.puts('Failed processing: ' + module);
+  //      queue.push(module);
+          return next();
+        }
+        util.puts('Processed: ' + module);
+        next();
+      });
+    }
+    
+    next();
   }
-  
-  next();
-}
+});
